a174909b4fa1dd2e7ced169246e45f4cd65e19cc
Yehor Tuchkov <yehor.tuchkov@visometry.com>
2023-08-26 00:40:39 +0200

    install-dependencies + README update

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Run `./install-dependencies.sh`. This does the following:
 - Install SDL, SDl_image, SDL_ttf source code and move it to YOURPROJECT/app/jni/.
 - Install externals for SDL_ttf
 
+Before building check, that you have the following folders installed:
+- (SDL/)[https://github.com/libsdl-org/SDL], (SDL_image/)[https://github.com/libsdl-org/SDL_image], (SDL_ttf/)[https://github.com/libsdl-org/SDL_ttf] folders in YOURPROJECT/app/jni/
+- SDl_ttf/external/ contains freetype/ and harfbuzz/ linked (here)[https://github.com/libsdl-org/SDL_ttf/tree/main/external]
+
+If you find some of these folders missing, you should download them manually from official repositories
+
 ## Building for Android
 Run `YOURPROJECT/gradlew installDebug` with connected Android device. Don't forget to enable developer mode and USB Debugging on it.
 
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,60 +4,64 @@
 SDL_VERSION="2.28.2"
 SDL_IMAGE_VERSION="2.6.3"
 SDL_TTF_VERSION="2.20.2"
-HARFBUZZ_VERSION="8.1.1"
-FREETYPE_VERSION="27"
-
-cd YOURPROJECT/app/jni/
+HARFBUZZ_VERSION="2.9.1"
+FREETYPE_VERSION="2.13.0"
 
 remove_directory() {
     if [ -d "$1" ]; then
+    echo "Removing directory: $1"
         rm -rf "$1"
     fi
 }
 
+remove_directory YOURPROJECT/.gradle
+remove_directory YOURPROJECT/app/build
+
+cd YOURPROJECT/app/jni/
+
 # Remove existing libraries if they exist
-remove_directory "SDL2"
-remove_directory "SDL2_image"
-remove_directory "SDL2_ttf"
+for dir in SDL*; do
+    remove_directory "$dir"
+done
 
 # Download and extract SDL2
 SDL_URL="https://www.libsdl.org/release/SDL2-$SDL_VERSION.zip"
 curl -o "SDL2-$SDL_VERSION.zip" -L "$SDL_URL"
-unzip -q "SDL2-$SDL_VERSION.zip" -d "SDL2_temp"
-mv SDL2_temp/* SDL2
-rm -rf SDL2_temp
+unzip -q "SDL2-$SDL_VERSION.zip" -d "SDL_temp"
+mv SDL_temp/* SDL
+rm -rf SDL_temp
 rm "SDL2-$SDL_VERSION.zip"
 
 # Download and extract SDL_image
 SDL_IMAGE_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-$SDL_IMAGE_VERSION.zip"
 curl -o "SDL2_image-$SDL_IMAGE_VERSION.zip" -L "$SDL_IMAGE_URL"
-unzip -q "SDL2_image-$SDL_IMAGE_VERSION.zip" -d "SDL2_image_temp"
-mv SDL2_image_temp/* SDL2_image
-rm -rf SDL2_image_temp/
+unzip -q "SDL2_image-$SDL_IMAGE_VERSION.zip" -d "SDL_image_temp"
+mv SDL_image_temp/* SDL_image
+rm -rf SDL_image_temp
 rm "SDL2_image-$SDL_IMAGE_VERSION.zip"
 
 # Download and extract SDL_ttf
 SDL_TTF_URL="https://github.com/libsdl-org/SDL_ttf/archive/refs/tags/release-$SDL_TTF_VERSION.zip"
 curl -o "SDL2_ttf-$SDL_TTF_VERSION.zip" -L "$SDL_TTF_URL"
-unzip -q "SDL2_ttf-$SDL_TTF_VERSION.zip" -d "SDL2_ttf_temp"
-mv SDL2_ttf_temp/* SDL2_ttf
-rm -rf SDL2_ttf_temp
+unzip -q "SDL2_ttf-$SDL_TTF_VERSION.zip" -d "SDL_ttf_temp"
+mv SDL_ttf_temp/* SDL_ttf
+rm -rf SDL_ttf_temp
 rm "SDL2_ttf-$SDL_TTF_VERSION.zip"
 
-cd "SDL2_ttf/external"
+cd "SDL_ttf/external"
 
 # Download and extract FreeType
-FREETYPE_URL="https://download.savannah.gnu.org/releases/freetype/ft$FREETYPE_VERSION.zip"
+FREETYPE_URL="https://github.com/libsdl-org/freetype/archive/fd08bbe13c52e305f037448e805e7e3c6ea1b1e4.zip"
 curl -o "freetype.zip" -L "$FREETYPE_URL"
 unzip -q "freetype.zip" -d "freetype_temp"
-mv freetype_temp/* freetype
+mv freetype_temp/*/* freetype
 rm -rf freetype_temp
 rm "freetype.zip"
 
 # Download and extract HarfBuzz
-HARFBUZZ_URL="https://github.com/harfbuzz/harfbuzz/archive/refs/tags/$HARFBUZZ_VERSION.zip"
+HARFBUZZ_URL="https://github.com/libsdl-org/harfbuzz/archive/43931e3e596c04044861770b831c8f9452e2d3b0.zip"
 curl -o "harfbuzz.zip" -L "$HARFBUZZ_URL"
 unzip -q "harfbuzz.zip" -d "harfbuzz_temp"
-mv harfbuzz_temp/* harfbuzz
+mv harfbuzz_temp/*/* harfbuzz
 rm -rf harfbuzz_temp
 rm "harfbuzz.zip"
